54cac45ec7b8037552372ee6eeb3d6f2ffde93d4
slaventy.69@gmail.com <Ginrihovich>
2022-10-18 10:12:22 +0300

    init notion lite

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import ContentLoader from 'react-content-loader';
+
+import List from './components/List';
+import TextSkeleton from './components/TextSkeleton';
+import Title from './components/Title';
 import './index.scss';
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+      <List />
+      <div className="content">
+        <Title />
+
+        <TextSkeleton />
       </div>
     </div>
   );
