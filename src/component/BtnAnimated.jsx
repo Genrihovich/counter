ce34691824313ddbf485a4165abf94c56a4d1a47
Slaventy1969 <slaventy.69@gmail.com>
2022-10-29 19:13:38 +0300

    Implementation Btn with animation

--- src/component/BtnAnimated.jsx
+++ src/component/BtnAnimated.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const BtnAnimated = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div className='wrapper'>
-      <button className="open-modal-btn">✨ Открыть окно с анимацией</button>
-      {/* <div className="overlay">
+      <button className="open-modal-btn" onClick={() => { setOpen(true) }}>✨ Открыть окно с анимацией</button>
+      <div className={`overlay animated ${open ? 'show' : ''}`}>
         <div className="modal">
-          <svg height="200" viewBox="0 0 200 200" width="200">
+          <svg height="200" viewBox="0 0 200 200" width="200" onClick={() => { setOpen(false) }}>
             <title />
             <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
           </svg>
           <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
         </div>
-      </div> */}
-    </div>
+      </div>
+    </div >
   )
 }
 export default BtnAnimated;
