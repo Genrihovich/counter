9f225356bb101fa9e925d2a0b4f65c0d4068419a
slaventy.69@gmail.com <Ginrihovich>
2022-11-02 13:52:36 +0200

    displaying a list of users

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.scss';
 import { Success } from './components/Success';
 import { Users } from './components/Users';
@@ -6,9 +6,29 @@ import { Users } from './components/Users';
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = useState([]); //массив юзеров
+  const [isLoading, setLoading] = useState(true); //идет загрузка
+
+  //отправляем запрос на бекенд
+  useEffect(() => {
+    fetch('https://reqres.in/api/users')
+      .then((resp) => resp.json())
+      .then((json) => {
+        setUsers(json.data)
+      }).catch((err) => {
+        console.warn(err);
+        alert('Ошибка при получении юзеров')
+      }).finally(() => setLoading(false)); //когда загружено прячем Sceleton
+  }, []);
+
+
+
   return (
     <div className="App">
-      <Users />
+      <Users
+        items={users}
+        isLoading={isLoading}
+      />
       {/* <Success /> */}
     </div>
   );
