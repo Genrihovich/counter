96570eb3b110b9a33db2bc62649ee04ba5b0baf5
slaventy.69@gmail.com <Ginrihovich>
2022-11-09 10:28:53 +0200

    refactoring: svg new component

--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 import { Skeleton } from './Skeleton';
 import { User } from './User';
 
+import { IconSearchSvg } from '../svg/IconSvg';
+
 export const Users = ({ items, isLoading, searchValue, onChangeSearchValue, invites, onInviteClick, onClickSendInvites }) => {
-    //console.log(invites);
-    //console.log(searchValue);
     return (
         <>
             <div className="search">
-                <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
-                </svg>
+                <IconSearchSvg />
                 <input
                     type="text"
                     placeholder="Найти пользователя..."
@@ -26,22 +24,18 @@ export const Users = ({ items, isLoading, searchValue, onChangeSearchValue, invi
                 </div>
             ) : (
                 <ul className="users-list">
-                    {
-                        items.filter(obj => {
-                            const fullName = obj.first_name + obj.last_name;
+                    {items.filter(obj => {
+                        const fullName = obj.first_name + obj.last_name;
 
-                            return (fullName.toLowerCase().includes(searchValue.toLowerCase()) ||
-                                (obj.email.includes(searchValue.toLowerCase())))
-                        })
-                            .map((obj) =>
-                                <User
-                                    isInvited={invites.includes(obj.id)} //проверяем или есть такое id в приглашенных
-                                    key={obj.id}
-                                    {...obj}
-
-                                    onInviteClick={onInviteClick}
-                                />)
-                    }
+                        return (fullName.toLowerCase().includes(searchValue.toLowerCase()) ||
+                            (obj.email.includes(searchValue.toLowerCase())))
+                    }).map((obj) =>
+                        <User
+                            isInvited={invites.includes(obj.id)} //проверяем или есть такое id в приглашенных
+                            key={obj.id}
+                            onInviteClick={onInviteClick}
+                            {...obj}
+                        />)}
                 </ul>
             )}
             {
